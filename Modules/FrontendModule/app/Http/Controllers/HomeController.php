46469127dfe4876ae633631e02d95e28bfee26a5
ahsanranga72 <ahsan.ranga@gmail.com>
2024-01-05 15:48:26 +0600

    student now can see follow teachers blog

--- Modules/FrontendModule/app/Http/Controllers/HomeController.php
+++ Modules/FrontendModule/app/Http/Controllers/HomeController.php
@@ -3,28 +3,47 @@
 namespace Modules\FrontendModule\app\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Modules\AdminModule\app\Models\Blog;
 use Modules\AdminModule\app\Models\Notice;
+use Modules\StudentModule\app\Models\FollowRequest;
 
 class HomeController extends Controller
 {
     private $notice;
     private $blog;
+    private $follow_request;
+    private $user;
 
-    public function __construct(Notice $notice, Blog $blog)
+    public function __construct(Notice $notice, Blog $blog, FollowRequest $follow_request, User $user)
     {
         $this->notice = $notice;
         $this->blog = $blog;
+        $this->follow_request = $follow_request;
+        $this->user = $user;
     }
     /**
      * Display a listing of the resource.
      */
     public function home()
     {
-        $blogs = $this->blog->with('owner')->active()->latest()->paginate(5);
+        $admin_ids = $this->user->active()->whereIn('user_type', ADMIN)->pluck('id')->toArray();
+
+        $blogs = $this->blog->with('owner')->active();
+        
+        if (auth()->check() && auth()->user()->user_type === STUDENT) {
+            $follow_teachers_user_id = $this->follow_request->where('student_user_id', auth()->user()->id)->pluck('teacher_user_id')->toArray();
+            $admin_teacher_ids = array_merge($admin_ids, $follow_teachers_user_id);
+
+            $blogs = $blogs->whereIn('created_by', $admin_teacher_ids);
+        } else {
+
+            $blogs = $blogs->whereIn('created_by', $admin_ids);
+        }
+        $blogs = $blogs->latest()->paginate(5);
         $notices = $this->notice->active()->latest()->get();
         return view('frontendmodule::home', compact('notices', 'blogs'));
     }
